1ccd2453ec6dd43c6e7d4f3efcf2ba48a9ae5a93
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-09-14 22:28:25 +0200

    Started 'users' model. Added 'jsonwebtoken', 'passport', 'passport-local' and 'envs' for storing and using environment variables.

--- bingo-api/controllers/authentication.js
+++ bingo-api/controllers/authentication.js
@@ -1,3 +1,10 @@
 /**
  * Created by Mihail on 9/14/2016.
  */
+module.exports.register = function(req, res) {
+	console.log("Registering user: " + req.body.email);
+	res.status(200);
+	res.json({
+		"message" : "User registered: " + req.body.email
+	});
+};
--- bingo-api/models/users.js
+++ bingo-api/models/users.js
@@ -1,3 +1,43 @@
 /**
  * Created by Mihail on 9/14/2016.
  */
+let crypto = require('kcrypto');
+let jwt = require('jsonwebtoken');
+let envs= require('.envs');
+
+
+let userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	hash: String,
+	salt: String
+});
+
+userSchema.methods.setPassword = function(password){
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+	let hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+	return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+	let expiry = new Date();
+	expiry.setDate(expiry.getDate() + 7);
+
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: parseInt(expiry.getTime() / 1000)
+	}, envs('DB_SECRET')); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
