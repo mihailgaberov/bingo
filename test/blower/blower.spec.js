292ead584269ac0eba43de5cda95d59eb5f5da8e
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-10-02 13:50:13 +0200

    Added unit test for the blower module - only the blower part, not testing the Ball creation functionality as it's coming from PaperJS.

--- test/blower/blower.spec.js
+++ test/blower/blower.spec.js
@@ -15,12 +15,38 @@ describe('Blower module', () => {
 
 	it ('Should create a blower module', (done) => {
 		let el = document.createElement('canvas');
-		el.setAttribute('id', 'blower');
 		let blower = new Blower(el);
 		done();
 	});
 
-	it ('Should contain an array with 75 balls', (done) => {
+	it ('Should init with stopped animation', (done) => {
+		let el = document.createElement('canvas');
+		let blower = new Blower(el);
+		expect(blower.init).to.be.object;
+		expect(blower.init).to.have.property('play').that.is.falsy;
+		expect(blower.init).to.have.property('isPlaying').that.is.falsy;
+		done();
+	});
 
+	it ('Should create an array with 75 ball objects', (done) => {
+		let el = document.createElement('canvas');
+		let blower = new Blower(el);
+		expect(blower.balls).to.be.an('array');
+		expect(blower.balls.length).to.be.eql(75);
+		done();
+	});
+
+	it ('Should have mechanism for starting the animation', (done) => {
+		let el = document.createElement('canvas');
+		let blower = new Blower(el);
+		expect(blower.startAnimation).to.be.function;
+		done();
+	});
+
+	it ('Should have mechanism for stopping the animation', (done) => {
+		let el = document.createElement('canvas');
+		let blower = new Blower(el);
+		expect(blower.stopAnimation).to.be.function;
+		done();
 	});
 });
