3df3d4a8692c2d444acf6a51c4a521e2af05c7c4
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-12-04 12:25:47 +0100

    Added back-office controller to contain all methods needed for managing the players. Implemented methods for getting the data for all registered players.

--- bingo-api/controllers/back-office.js
+++ bingo-api/controllers/back-office.js
@@ -1,10 +1,16 @@
 /**
  * Created by Mihail on 12/4/2016.
  */
+"use strict";
+
 var mongoose = require('mongoose');
 var User = mongoose.model('Users');
 
 module.exports.getAllUsers = function (req, res) {
-
+	User
+		.find()
+		.exec(function (err, allUsers) {
+			res.status(200).json(allUsers);
+		});
 };
 
