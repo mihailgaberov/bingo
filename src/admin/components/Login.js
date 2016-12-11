0f608f9cf0d158c3dc25c9dfacef5d17c1873d43
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-12-11 16:43:08 +0100

    Added more styles for the login form in the back office.

--- src/admin/components/Login.js
+++ src/admin/components/Login.js
@@ -55,7 +55,7 @@ class Login extends React.Component {
 				Login
 			</Button>
 			<footer>
-				© 2016 Copyrights <a href="mailto: me@mihail-gaberov.eu">Mihail Gaberov</a>.
+				<span>© 2016 Copyrights <a href="mailto: me@mihail-gaberov.eu">Mihail Gaberov</a>.</span>
 			</footer>
 		</div>
 	}
