fdb8a7c34cafb0a57467e5839c53969a16e5ac9a
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-02-25 19:27:03 +0100

    Main App class refactoring - extracted the logic for preparing the game into a separated class Initializator. Amended the unit tests for App component.

--- src/app.js
+++ src/app.js
@@ -3,24 +3,15 @@
  */
 'use strict';
 
-import ApiController from './api/api-controller';
-import ViewManipulator from './utils/view-manipulator';
-import CardGenerator from './card/card-generator';
-import CardDrawer from './card/card-drawer';
-import MarketCards from './market-place/market-cards';
-import Blower from './blower/blower';
-import Dauber from './dauber/dauber';
-import Timer from './utils/timer';
-import { EventsConsts } from './events/events-consts';
-import WinningDialog from './winning/winning-dialog';
 import 'es6-promise';
 import 'isomorphic-fetch';
 import { ApiConsts } from './api/api-consts';
-import WinPatternsAnimModule from './winning/win-patterns-anim-module';
+import Initializator from './initializator/initializer';
 
 class App {
 	constructor() {
 		this.confUrl = ApiConsts.CONF;
+    this.initializator = null;
 
     fetch(this.confUrl).then((response) => {
       if (response.status >= 400) {
@@ -28,84 +19,9 @@ class App {
       }
       return response.json();
     }).then((config) => {
-      this.init(config);
+      this.initializator = new Initializator(config);
     });
 	}
-
-	init(conf) {
-		this.title = conf.gameConf.appTitle;
-		document.querySelector('title').innerText = this.title;
-		const elMarketPlace = document.querySelector('#marketPlace');
-
-		// Create the components only if they are allowed in the config
-		if (conf.gameConf.winningDialog) {
-			const winningDialog = new WinningDialog('#winningDialogContainer');
-		}
-
-		if (!conf.gameConf.marketCards) {
-			ViewManipulator.toggleVisibility(elMarketPlace, false);
-		}
-		MarketCards.setCardPrices(conf.gameConf.cardPrice, document.querySelectorAll('.cards'));
-
-		let elWinPatternsAnimModule = null;
-		if (conf.gameConf.winPatternsAnimModule) {
-			elWinPatternsAnimModule = document.querySelector('#winPatternsAnimModule');
-			const horPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#horizontal'), 5, 5, 'horizontal');
-			const verPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#vertical'), 5, 5, 'vertical');
-			const diagPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#diagonal'), 5, 5, 'diagonal');
-		}
-
-		if (conf.gameConf.mainGame) {
-			const timer = new Timer(
-				document.querySelector('#timerContainer'),
-				conf.gameConf.beforeStartGameSeconds,
-				EventsConsts.START_GAME, true
-			);
-
-			document.addEventListener(EventsConsts.ENOUGH_BALANCE, () => {
-				ViewManipulator.toggleVisibility(elMarketPlace, false);
-				ViewManipulator.toggleVisibility(document.querySelector('#footer'), false);
-				timer.pulsate();
-			});
-
-			const startBtn = document.querySelector('#startBtn');
-			if (startBtn) {
-				startBtn.addEventListener('click', (e) => {
-					this.initPlayingCards(conf, elMarketPlace);
-				});
-			}
-		}
-
-		if (conf.gameConf.dauber) {
-			const dauber = new Dauber(conf, document.querySelector('#tube'));
-			const blower = new Blower(document.querySelector('#blower-balloon'));
-		}
-
-		const apiCtrl = new ApiController();
-		const logoutBtn = document.querySelector('#logoutBtn');
-		if (logoutBtn) {
-			logoutBtn.addEventListener('click', (e) => {
-				apiCtrl.logout();
-			});
-		}
-
-		if (ApiController.isLogged()) {
-			ViewManipulator.showUserInfo();
-		}
-	}
-
-  initPlayingCards(conf, container) {
-    if (conf.gameConf.playingCards) {
-      this.cardGen = new CardGenerator(conf);
-      const marketCards = new MarketCards(container);
-      const purchasedCardsCount = MarketCards.getPurchasedCardsCount(marketCards.getRadioButtonsArray());
-      const cardDrawer = new CardDrawer(
-        this.cardGen.generateCards(purchasedCardsCount),
-        document.querySelector('#leftGameScreen')
-      );
-      MarketCards.buyCards(purchasedCardsCount, conf.gameConf.cardPrice);
-    }
-  }
 }
 
 export default App;
---
+++ test/initializator/initializer.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Mihail on 2/25/2017.
+ */
+'use strict';
+
+import Initializer from '../../src/initializator/initializer';
+import { expect, assert } from 'chai';
+
+describe('Bingo App Initializer', () => {
+
+});
