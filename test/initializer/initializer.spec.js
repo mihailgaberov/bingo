bfe12096b08eb6c7e8c1d976a5de413e7e829bc6
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-02-25 19:46:18 +0100

    Renamed Initializator to Initializer. Started refactoring on it and fulfilling with unit tests.

---
+++ test/initializer/initializer.spec.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Mihail on 2/25/2017.
+ */
+'use strict';
+
+import Initializer from '../../src/initializer/initializer';
+import { expect, assert } from 'chai';
+
+describe('Bingo App Initializer', () => {
+  /*it('Should set the title of the application', () => {
+   expect(appBingo.title).to.be.a('string');
+   setTimeout(() => {
+   expect(appBingo.title).to.equal('Welcome To Bingo Bigul');
+   }, 200);
+   });*/
+
+  /*it('Should initialize the CardGenerator to init the app', () => {
+   expect(appBingo.hasOwnProperty(appBingo.cardGen)).not.to.be.undefined;
+   });
+
+   it('Should initialize the CardDrawer to init the app', () => {
+   expect(appBingo.hasOwnProperty(appBingo.CardDrawer)).not.to.be.undefined;
+   });*/
+});
