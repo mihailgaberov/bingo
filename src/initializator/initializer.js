93c365f1428e598be907804e735edeee54353b7d
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-02-25 19:27:03 +0100

    Main App class refactoring - extracted the logic for preparing the game into a separated class Initializator. Amended the unit tests for App component.

---
+++ src/initializator/initializer.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Mihail on 2/25/2017.
+ */
+'use strict';
+
+import ApiController from '../api/api-controller';
+import ViewManipulator from '../utils/view-manipulator';
+import CardGenerator from '../card/card-generator';
+import CardDrawer from '../card/card-drawer';
+import MarketCards from '../market-place/market-cards';
+import Blower from '../blower/blower';
+import Dauber from '../dauber/dauber';
+import Timer from '../utils/timer';
+import { EventsConsts } from '../events/events-consts';
+import WinningDialog from '../winning/winning-dialog';
+import WinPatternsAnimModule from '../winning/win-patterns-anim-module';
+
+class Initializator {
+  constructor(conf) {
+    this.title = conf.gameConf.appTitle;
+    document.querySelector('title').innerText = this.title;
+
+    const elMarketPlace = document.querySelector('#marketPlace');
+
+    if (conf.gameConf.winningDialog) {
+      const winningDialog = new WinningDialog('#winningDialogContainer');
+    }
+
+    if (!conf.gameConf.marketCards) {
+      ViewManipulator.toggleVisibility(elMarketPlace, false);
+    }
+
+    MarketCards.setCardPrices(conf.gameConf.cardPrice, document.querySelectorAll('.cards'));
+
+    let elWinPatternsAnimModule = null;
+    if (conf.gameConf.winPatternsAnimModule) {
+      elWinPatternsAnimModule = document.querySelector('#winPatternsAnimModule');
+      const horPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#horizontal'), 5, 5, 'horizontal');
+      const verPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#vertical'), 5, 5, 'vertical');
+      const diagPattern = new WinPatternsAnimModule(elWinPatternsAnimModule.querySelector('#diagonal'), 5, 5, 'diagonal');
+    }
+
+    if (conf.gameConf.mainGame) {
+      const timer = new Timer(
+        document.querySelector('#timerContainer'),
+        conf.gameConf.beforeStartGameSeconds,
+        EventsConsts.START_GAME, true
+      );
+
+      document.addEventListener(EventsConsts.ENOUGH_BALANCE, () => {
+        ViewManipulator.toggleVisibility(elMarketPlace, false);
+        ViewManipulator.toggleVisibility(document.querySelector('#footer'), false);
+        timer.pulsate();
+      });
+
+      const startBtn = document.querySelector('#startBtn');
+      if (startBtn) {
+        startBtn.addEventListener('click', (e) => {
+          this.initPlayingCards(conf, elMarketPlace);
+        });
+      }
+    }
+
+    if (conf.gameConf.dauber) {
+      const dauber = new Dauber(conf, document.querySelector('#tube'));
+      const blower = new Blower(document.querySelector('#blower-balloon'));
+    }
+
+    const apiCtrl = new ApiController();
+
+    const logoutBtn = document.querySelector('#logoutBtn');
+    if (logoutBtn) {
+      logoutBtn.addEventListener('click', (e) => {
+        apiCtrl.logout();
+      });
+    }
+
+    if (ApiController.isLogged()) {
+      ViewManipulator.showUserInfo();
+    }
+  }
+
+
+  initPlayingCards(conf, container) {
+    if (conf.gameConf.playingCards) {
+      this.cardGen = new CardGenerator(conf);
+      const marketCards = new MarketCards(container);
+      const purchasedCardsCount = MarketCards.getPurchasedCardsCount(marketCards.getRadioButtonsArray());
+      const cardDrawer = new CardDrawer(
+        this.cardGen.generateCards(purchasedCardsCount),
+        document.querySelector('#leftGameScreen')
+      );
+      MarketCards.buyCards(purchasedCardsCount, conf.gameConf.cardPrice);
+    }
+  }
+}
+
+export default Initializator;
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -4,7 +4,7 @@
 'use strict';
 import App from '../../src/app';
 import { expect, assert } from 'chai';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 
 describe('Bingo App', () => {
 
@@ -15,27 +15,25 @@ describe('Bingo App', () => {
 		expect(appBingo.confUrl).to.be.a('string');
 	});
 
-	it('Should set the title of the application', () => {
+  it('Should initialize the game via Game Initializator', () => {
+    setTimeout(() => {
+      expect(appBingo.initializator).not.to.be.null;
+    });
+  });
+
+
+	/*it('Should set the title of the application', () => {
 		expect(appBingo.title).to.be.a('string');
 		setTimeout(() => {
 			expect(appBingo.title).to.equal('Welcome To Bingo Bigul');
 		}, 200);
-	});
-
-	/*it("Should start game initialization when get the configs", function () {
-		let callback = sinon.spy();
-		let proxy = appBingo.init({});
-
-		proxy();
-
-		assert(callback.called);
 	});*/
 
-	it('Should initialize the CardGenerator to init the app', () => {
+	/*it('Should initialize the CardGenerator to init the app', () => {
 		expect(appBingo.hasOwnProperty(appBingo.cardGen)).not.to.be.undefined;
 	});
 
 	it('Should initialize the CardDrawer to init the app', () => {
 		expect(appBingo.hasOwnProperty(appBingo.CardDrawer)).not.to.be.undefined;
-	});
+	});*/
 });
