eb00bee9e928f14d633e845f98010a4b8f6807f6
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-02-25 16:49:36 +0100

    Main App class refactoring - removed mediate init function, moved loading of the config into the constructor.

--- test/app.spec.js
+++ test/app.spec.js
@@ -10,32 +10,32 @@ describe('Bingo App', () => {
 
 	let appBingo = new App();
 
-	it('Should set the url for loading the configs', () => {
+	it('Should have a url for loading the configs', () => {
 		expect(appBingo.confUrl).to.not.be.undefined;
 		expect(appBingo.confUrl).to.be.a('string');
 	});
 
 	it('Should set the title of the application', () => {
-		expect(appBingo.confUrl).to.be.a('string');
+		expect(appBingo.title).to.be.a('string');
 		setTimeout(() => {
 			expect(appBingo.title).to.equal('Welcome To Bingo Bigul');
 		}, 200);
 	});
 
-	it("Should call the callback when get the configs", function () {
+	/*it("Should start game initialization when get the configs", function () {
 		let callback = sinon.spy();
-		let proxy = appBingo.loadConfigs(callback);
+		let proxy = appBingo.init({});
 
 		proxy();
 
 		assert(callback.called);
-	});
+	});*/
 
-	it('Should initialize the CardGenerator to start the app', () => {
+	it('Should initialize the CardGenerator to init the app', () => {
 		expect(appBingo.hasOwnProperty(appBingo.cardGen)).not.to.be.undefined;
 	});
 
-	it('Should initialize the CardDrawer to start the app', () => {
+	it('Should initialize the CardDrawer to init the app', () => {
 		expect(appBingo.hasOwnProperty(appBingo.CardDrawer)).not.to.be.undefined;
 	});
 });
