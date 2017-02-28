e7568a33ded4fabfa0de71831188390ce9030be7
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-02-28 15:11:47 +0100

    Introduced Page Object methodology for end-to-end testing. Refactored the first e2e tests for login page to use it.

--- test/e2e/logingroup/login-form.spec.js
+++ test/e2e/logingroup/login-form.spec.js
@@ -2,12 +2,24 @@
  * Created by Mihail on 2/28/2017.
  */
 module.exports = {
-  'Login page' : function (browser) {
-    browser
-      .url('http://localhost:8000')
-      .waitForElementVisible('body', 1000)
+  'Login page': function (client) {
+    const loginPage = client.page.loginPage();
+
+    loginPage.navigate()
       .assert.title('Welcome to Bingo Bigul')
-      .assert.elementPresent('.brand-img')
-      .end();
+      .assert.visible('@logo')
+      .assert.visible('@title')
+      .assert.visible('@emailInput')
+      .assert.visible('@passwordInput')
+      .assert.visible('@loginBtn')
+      .assert.visible('@signInLink')
+      .assert.visible('@githubLogo')
+      .assert.visible('@linkedInLogo')
+      .assert.visible('@twitterLogo')
+      .assert.visible('@facebookLogo')
+      .setValue('@emailInput', 'mihail.gaberov@gmail.com')
+      .setValue('@passwordInput', 'asd')
+      .click('@loginBtn');
+    client.end();
   }
 };
