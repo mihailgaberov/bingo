def9774a32e3af8716166707ed82e13fed31265c
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-30 16:06:07 +0200

    :hammer: Migrating front end unit tests from Mocha to Jest - updated card.spec.js

--- front-end-tests/__tests__/card/card.spec.js
+++ front-end-tests/__tests__/card/card.spec.js
@@ -1,11 +1,4 @@
-/**
- * Created by Mihail on 10/14/2016.
- */
-
-'use strict';
-
 import Card from '../../../src/card/card';
-import { expect } from 'chai';
 
 describe('Card object', () => {
 	const objCard = {
@@ -18,50 +11,50 @@ describe('Card object', () => {
 
 	test('Should create new HTML card', () => {
 		const card = new Card(objCard);
-		expect(card).not.to.be.undefined;
+		expect(card).toBeTruthy();
 	});
 
 	test('Should provide method for marking numbers', () => {
-		expect(Card.markNumber).not.to.be.undefined;
+		expect(Card.markNumber).toBeTruthy();
 	});
 
 	test('Should provide method for marking number cells as drawn', () => {
-		expect(Card.markDrawnNumber).not.to.be.undefined;
+		expect(Card.markDrawnNumber).toBeTruthy();
 	});
 
 	test(
         'Should create new HTML card that has table cells with appropriate IDs',
         () => {
             const card = new Card(objCard);
-            expect(card.children[0].firstElementChild.children[1].children[0].id).to.be.equal('11');
-            expect(card.children[0].firstElementChild.children[1].children[1].id).to.be.equal('21');
-            expect(card.children[0].firstElementChild.children[1].children[2].id).to.be.equal('31');
-            expect(card.children[0].firstElementChild.children[1].children[3].id).to.be.equal('41');
-            expect(card.children[0].firstElementChild.children[1].children[4].id).to.be.equal('51');
+            expect(card.children[0].firstElementChild.children[1].children[0].id).toEqual('11');
+            expect(card.children[0].firstElementChild.children[1].children[1].id).toEqual('21');
+            expect(card.children[0].firstElementChild.children[1].children[2].id).toEqual('31');
+            expect(card.children[0].firstElementChild.children[1].children[3].id).toEqual('41');
+            expect(card.children[0].firstElementChild.children[1].children[4].id).toEqual('51');
 
-            expect(card.children[0].firstElementChild.children[2].children[0].id).to.be.equal('12');
-            expect(card.children[0].firstElementChild.children[2].children[1].id).to.be.equal('22');
-            expect(card.children[0].firstElementChild.children[2].children[2].id).to.be.equal('32');
-            expect(card.children[0].firstElementChild.children[2].children[3].id).to.be.equal('42');
-            expect(card.children[0].firstElementChild.children[2].children[4].id).to.be.equal('52');
+            expect(card.children[0].firstElementChild.children[2].children[0].id).toEqual('12');
+            expect(card.children[0].firstElementChild.children[2].children[1].id).toEqual('22');
+            expect(card.children[0].firstElementChild.children[2].children[2].id).toEqual('32');
+            expect(card.children[0].firstElementChild.children[2].children[3].id).toEqual('42');
+            expect(card.children[0].firstElementChild.children[2].children[4].id).toEqual('52');
 
-            expect(card.children[0].firstElementChild.children[3].children[0].id).to.be.equal('13');
-            expect(card.children[0].firstElementChild.children[3].children[1].id).to.be.equal('23');
-            expect(card.children[0].firstElementChild.children[3].children[2].id).to.be.equal('33');
-            expect(card.children[0].firstElementChild.children[3].children[3].id).to.be.equal('43');
-            expect(card.children[0].firstElementChild.children[3].children[4].id).to.be.equal('53');
+            expect(card.children[0].firstElementChild.children[3].children[0].id).toEqual('13');
+            expect(card.children[0].firstElementChild.children[3].children[1].id).toEqual('23');
+            expect(card.children[0].firstElementChild.children[3].children[2].id).toEqual('33');
+            expect(card.children[0].firstElementChild.children[3].children[3].id).toEqual('43');
+            expect(card.children[0].firstElementChild.children[3].children[4].id).toEqual('53');
 
-            expect(card.children[0].firstElementChild.children[4].children[0].id).to.be.equal('14');
-            expect(card.children[0].firstElementChild.children[4].children[1].id).to.be.equal('24');
-            expect(card.children[0].firstElementChild.children[4].children[2].id).to.be.equal('34');
-            expect(card.children[0].firstElementChild.children[4].children[3].id).to.be.equal('44');
-            expect(card.children[0].firstElementChild.children[4].children[4].id).to.be.equal('54');
+            expect(card.children[0].firstElementChild.children[4].children[0].id).toEqual('14');
+            expect(card.children[0].firstElementChild.children[4].children[1].id).toEqual('24');
+            expect(card.children[0].firstElementChild.children[4].children[2].id).toEqual('34');
+            expect(card.children[0].firstElementChild.children[4].children[3].id).toEqual('44');
+            expect(card.children[0].firstElementChild.children[4].children[4].id).toEqual('54');
 
-            expect(card.children[0].firstElementChild.children[5].children[0].id).to.be.equal('15');
-            expect(card.children[0].firstElementChild.children[5].children[1].id).to.be.equal('25');
-            expect(card.children[0].firstElementChild.children[5].children[2].id).to.be.equal('35');
-            expect(card.children[0].firstElementChild.children[5].children[3].id).to.be.equal('45');
-            expect(card.children[0].firstElementChild.children[5].children[4].id).to.be.equal('55');
+            expect(card.children[0].firstElementChild.children[5].children[0].id).toEqual('15');
+            expect(card.children[0].firstElementChild.children[5].children[1].id).toEqual('25');
+            expect(card.children[0].firstElementChild.children[5].children[2].id).toEqual('35');
+            expect(card.children[0].firstElementChild.children[5].children[3].id).toEqual('45');
+            expect(card.children[0].firstElementChild.children[5].children[4].id).toEqual('55');
         }
     );
 });
