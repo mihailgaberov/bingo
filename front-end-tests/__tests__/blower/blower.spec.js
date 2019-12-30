80be9b70ac27b44e5c723c23c9da4386c86929d4
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-30 17:16:59 +0200

    :heavy_check_mark: Migrating front end unit tests from Mocha to Jest - updated ball.spec.js

--- front-end-tests/__tests__/blower/blower.spec.js
+++ front-end-tests/__tests__/blower/blower.spec.js
@@ -1,35 +1,35 @@
 import Blower from '../../../src/blower/blower';
 
 describe('Blower module', () => {
-  test('Should create a blower component', async (done) => {
+  test('Should create a blower component', (done) => {
     let el = document.createElement('canvas');
     const blower = new Blower(el);
     expect(blower).not.toBe(undefined);
     done();
   });
 
-  test('Should init with stopped animation', async (done) => {
+  test('Should init with stopped animation', (done) => {
     let el = document.createElement('canvas');
     let blower = new Blower(el);
     expect(blower.init).toEqual(expect.objectContaining({ play: false, isPlaying: false }));
     done();
   });
 
-  test('Should create an array with 75 ball objects',async (done) => {
+  test('Should create an array with 75 ball objects', (done) => {
     let el = document.createElement('canvas');
     let blower = new Blower(el);
     expect(blower.balls.length).toEqual(75);
     done();
   });
 
-  test('Should have mechanism for starting the animation', async (done) => {
+  test('Should have mechanism for starting the animation', (done) => {
     let el = document.createElement('canvas');
     let blower = new Blower(el);
     expect(typeof blower.startAnimation).toBe('function');
     done();
   });
 
-  test('Should have mechanism for stopping the animation', async (done) => {
+  test('Should have mechanism for stopping the animation', (done) => {
     let el = document.createElement('canvas');
     let blower = new Blower(el);
     expect(typeof blower.stopAnimation).toBe('function');
--- front-end-tests/__tests__/dauber/ball.spec.js
+++ front-end-tests/__tests__/dauber/ball.spec.js
@@ -1,51 +1,47 @@
-/**
- * Created by Mihail on 10/2/2016.
- */
-'use strict';
-
 import Ball from '../../../src/dauber/ball';
 import PubSub from '../../../src/events/pubsub-service';
-import { expect } from 'chai';
+
+jest.useFakeTimers();
 
 describe('Ball module', () => {
-	test('Should create a ball object', (done) => {
-		const pb = new PubSub();
-		const ball = new Ball(34, pb, 'original');
-		expect(ball).not.to.be.undefined;
-		done();
+
+  test('Should create a ball object', (done) => {
+    const pb = new PubSub();
+    const ball = new Ball(34, pb, 'original');
+    expect(ball).toBeTruthy();
+    done();
+  });
+
+  test('Should create a ball object with given number', (done) => {
+    const pb = new PubSub();
+    const ball = new Ball(34, pb, 'original');
+    expect(ball.elNumber.innerText).toBeTruthy();
+    expect(ball.elNumber.innerText).toEqual(34);
+    done();
+  });
+
+  test('Should create a ball object with given css class', (done) => {
+    const pb = new PubSub();
+    const ball = new Ball(34, pb, { name: 'original' });
+    expect(ball.elBall.className).toEqual('original_ballN');
+    done();
+  });
+
+  test('Should be able to draw itself', () => {
+    const pb = new PubSub();
+    const ball = new Ball(34, pb, 'original');
+    expect(ball.draw).toBeTruthy();
+    const el = document.createElement('div');
+    el.setAttribute('id', '#tube');
+		ball.draw(el, 4, true);
+		expect(setTimeout).toHaveBeenCalledTimes(1);
 	});
 
-	test('Should create a ball object with given number', (done) => {
-		const pb = new PubSub();
-		const ball = new Ball(34, pb, 'original');
-		expect(ball.elNumber.innerText).not.to.be.undefined;
-		expect(ball.elNumber.innerText).to.be.equal(34);
-		done();
-	});
-
-	test('Should create a ball object with given css class', (done) => {
-		const pb = new PubSub();
-		const ball = new Ball(34, pb, { name: 'original' });
-		expect(ball.elBall.className).to.be.equal('original_ballN');
-		done();
-	});
-
-	test('Should be able to draw itself', (done) => {
-		const pb = new PubSub();
-		const ball = new Ball(34, pb, 'original');
-		expect(ball.draw).not.to.be.undefined;
-		const el = document.createElement('div');
-		el.setAttribute('id', 'tube');
-		ball.draw(el);
-		expect(document.querySelector('#tube')).to.have.children;
-		done();
-	});
-
-	test('Should have methods moveVerticalHorizontal and animate', (done) => {
-		const pb = new PubSub();
-		const ball = new Ball(34, pb, 'original');
-		expect(ball.move).not.to.be.undefined;
-		expect(ball.animate).not.to.be.undefined;
-		done();
-	});
+  test('Should have methods moveVerticalHorizontal and animate', (done) => {
+    const pb = new PubSub();
+    const ball = new Ball(34, pb, 'original');
+    expect(ball.move).toBeTruthy();
+    expect(ball.animate).toBeTruthy();
+    done();
+  });
 });
