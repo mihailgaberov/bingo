20ac4ed0d34eb97356ae9a4b2c66f5f56b67c614
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-30 13:26:35 +0200

    :hammer: Migrating front end unit tests from Mocha to Jest - updated events-consts.spec.js.

--- front-end-tests/__tests__/app.spec.js
+++
@@ -1,8 +0,0 @@
-// TODO: to review - what are we testing here - app.js or initializer.applyConfigurations
-import Initializer from '../../src/initializer/initializer';
-
-describe('Bingo App', () => {
-  test('Should initialize the game via Game Initializer', () => {
-    expect(Initializer.applyConfigurations).to.be.called;
-  });
-});
--- front-end-tests/__tests__/events/events-consts.spec.js
+++ front-end-tests/__tests__/events/events-consts.spec.js
@@ -3,38 +3,38 @@ import { EventsConsts } from '../../../src/events/events-consts';
 describe('Bingo Events Constants', () => {
 
 	test('Should contain LOGOUT constant', () => {
-		expect(EventsConsts.LOGOUT).to.be.equal('logout');
+		expect(EventsConsts.LOGOUT).toEqual('logout');
 	});
 
 	test('Should contain NEW_BALL_DRAWN constant', () => {
-		expect(EventsConsts.NEW_BALL_DRAWN).to.be.equal('newBallDrawn');
+		expect(EventsConsts.NEW_BALL_DRAWN).toEqual('newBallDrawn');
 	});
 
 	test('Should contain START_GAME constant', () => {
-		expect(EventsConsts.START_GAME).to.be.equal('startGame');
+		expect(EventsConsts.START_GAME).toEqual('startGame');
 	});
 
 	test('Should contain END_GAME constant', () => {
-		expect(EventsConsts.END_GAME).to.be.equal('endGame');
+		expect(EventsConsts.END_GAME).toEqual('endGame');
 	});
 
 	test('Should contain BINGO constant', () => {
-		expect(EventsConsts.BINGO).to.be.equal('bingo');
+		expect(EventsConsts.BINGO).toEqual('bingo');
 	});
 
 	test('Should contain PRIZE_WON constant', () => {
-		expect(EventsConsts.PRIZE_WON).to.be.equal('prizeWon');
+		expect(EventsConsts.PRIZE_WON).toEqual('prizeWon');
 	});
 
 	test('Should contain FLYING_PRIZE_ANIMATION_ENDS constant', () => {
-		expect(EventsConsts.FLYING_PRIZE_ANIMATION_ENDS).to.be.equal('flyingPrizeAnimationEnds');
+		expect(EventsConsts.FLYING_PRIZE_ANIMATION_ENDS).toEqual('flyingPrizeAnimationEnds');
 	});
 
 	test('Should contain ENOUGH_BALANCE constant', () => {
-		expect(EventsConsts.ENOUGH_BALANCE).to.be.equal('enoughBalance');
+		expect(EventsConsts.ENOUGH_BALANCE).toEqual('enoughBalance');
 	});
 
 	test('Should contain NOT_ENOUGH_BALANCE constant', () => {
-		expect(EventsConsts.NOT_ENOUGH_BALANCE).to.be.equal('notEnoughBalance');
+		expect(EventsConsts.NOT_ENOUGH_BALANCE).toEqual('notEnoughBalance');
 	});
 });
