177301e997fa6bdba036e9fe605d2eba0208b5e5
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-30 15:47:16 +0200

    :hammer: Migrating front end unit tests from Mocha to Jest - updated blower.spec.js

--- front-end-tests/__tests__/blower/blower.spec.js
+++ front-end-tests/__tests__/blower/blower.spec.js
@@ -1,41 +1,38 @@
 import Blower from '../../../src/blower/blower';
 
 describe('Blower module', () => {
-	test('Should create a blower component', async (done) => {
-		let el = document.createElement('canvas');
-		const blower = new Blower(el);
-		expect(blower).not.toBe(undefined);
-		done();
-	});
+  test('Should create a blower component', async (done) => {
+    let el = document.createElement('canvas');
+    const blower = new Blower(el);
+    expect(blower).not.toBe(undefined);
+    done();
+  });
 
-	test('Should init with stopped animation', async (done) => {
-		let el = document.createElement('canvas');
-		let blower = new Blower(el);
-		expect(blower.init).toBe(object);
-		expect(blower.init).to.have.property('play').that.is.falsy;
-		expect(blower.init).to.have.property('isPlaying').that.is.falsy;
-		done();
-	});
+  test('Should init with stopped animation', async (done) => {
+    let el = document.createElement('canvas');
+    let blower = new Blower(el);
+    expect(blower.init).toEqual(expect.objectContaining({ play: false, isPlaying: false }));
+    done();
+  });
 
-	test('Should create an array with 75 ball objects', (done) => {
-		let el = document.createElement('canvas');
-		let blower = new Blower(el);
-		expect(blower.balls).to.be.an('array');
-		expect(blower.balls.length).to.be.eql(75);
-		done();
-	});
+  test('Should create an array with 75 ball objects',async (done) => {
+    let el = document.createElement('canvas');
+    let blower = new Blower(el);
+    expect(blower.balls.length).toEqual(75);
+    done();
+  });
 
-	test('Should have mechanism for starting the animation', (done) => {
-		let el = document.createElement('canvas');
-		let blower = new Blower(el);
-		expect(blower.startAnimation).to.be.function;
-		done();
-	});
+  test('Should have mechanism for starting the animation', async (done) => {
+    let el = document.createElement('canvas');
+    let blower = new Blower(el);
+    expect(typeof blower.startAnimation).toBe('function');
+    done();
+  });
 
-	test('Should have mechanism for stopping the animation', (done) => {
-		let el = document.createElement('canvas');
-		let blower = new Blower(el);
-		expect(blower.stopAnimation).to.be.function;
-		done();
-	});
+  test('Should have mechanism for stopping the animation', async (done) => {
+    let el = document.createElement('canvas');
+    let blower = new Blower(el);
+    expect(typeof blower.stopAnimation).toBe('function');
+    done();
+  });
 });
