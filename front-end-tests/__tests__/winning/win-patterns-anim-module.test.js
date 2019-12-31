d99a00b9c19b80c36b4417b0f609cf4d8e8b21be
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-31 14:32:29 +0200

    :heavy_check_mark: Migrating front end unit tests from Mocha to Jest - updated win-patterns-anim-module.test.js

--- front-end-tests/__tests__/winning/win-patterns-anim-module.test.js
+++ front-end-tests/__tests__/winning/win-patterns-anim-module.test.js
@@ -1,60 +1,56 @@
-/**
- * Created by Mihail on 11/24/2016.
- */
-'use strict';
-
 import WinPatternsAnimModule from '../../../src/winning/win-patterns-anim-module';
-import { expect } from 'chai';
 
 describe('WinPatternsAnimModule module', () => {
-	test('Should get element container, rows, cols and patterns', () => {
-		const container = document.createElement('section');
-		let wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
-		expect(wpam.elem).not.to.be.undefined;
-		expect(wpam.rows).not.to.be.undefined;
-		expect(wpam.cols).not.to.be.undefined;
-		expect(wpam.pattern).not.to.be.undefined;
-	});
+  test('Should get element container, rows, cols and patterns', () => {
+    const container = document.createElement('section');
+    let wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
+    expect(wpam.elem).toBeDefined();
+    expect(wpam.rows).toBeDefined();
+    expect(wpam.cols).toBeDefined();
+    expect(wpam.pattern).toBeDefined();
+  });
 
-	test(
-        'Should call createDomElement to create the DOM structure of the module',
-        () => {
-            const container = document.createElement('section');
-            const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
-            expect(wpam.createDomElement).to.be.calledOnce;
-        }
-    );
+  test(
+    'Should call createDomElement to create the DOM structure of the module',
+    () => {
+      const container = document.createElement('section');
+      const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
+      wpam.createDomElement = jest.fn();
+      setTimeout(() => expect(wpam.createDomElement).toHaveBeenCalled());
+    }
+  );
 
-	test(
-        'Should call startAnimation method to animate the winning patterns',
-        () => {
-            const container = document.createElement('section');
-            const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
-            expect(wpam.startAnimation).to.be.calledOnce;
-        }
-    );
+  test(
+    'Should call startAnimation method to animate the winning patterns',
+    () => {
+      const container = document.createElement('section');
+      const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
+			wpam.startAnimation = jest.fn();
+			setTimeout(() => expect(wpam.startAnimation).toHaveBeenCalled());
+    }
+  );
 
-	test('Should be able to clear the animation module table', () => {
-		const container = document.createElement('section');
-		const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
-		expect(wpam.clearTable).not.to.be.undefined;
-	});
+  test('Should be able to clear the animation module table', () => {
+    const container = document.createElement('section');
+    const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
+    expect(wpam.clearTable).toBeDefined();
+  });
 
-	test('Should have method for animating horizontal pattern', () => {
-		const container = document.createElement('section');
-		const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
-		expect(wpam.startHorizontalAnim).not.to.be.undefined;
-	});
+  test('Should have method for animating horizontal pattern', () => {
+    const container = document.createElement('section');
+    const wpam = new WinPatternsAnimModule(container, 5, 5, 'horizontal');
+    expect(wpam.startHorizontalAnim).toBeDefined();
+  });
 
-	test('Should have method for animating vertical pattern', () => {
-		const container = document.createElement('section');
-		const wpam = new WinPatternsAnimModule(container, 5, 5, 'vertical');
-		expect(wpam.startVerticalAnim).not.to.be.undefined;
-	});
+  test('Should have method for animating vertical pattern', () => {
+    const container = document.createElement('section');
+    const wpam = new WinPatternsAnimModule(container, 5, 5, 'vertical');
+    expect(wpam.startVerticalAnim).toBeDefined();
+  });
 
-	test('Should have method for animating diagonal pattern', () => {
-		const container = document.createElement('section');
-		const wpam = new WinPatternsAnimModule(container, 5, 5, 'diagonal');
-		expect(wpam.startDiagonallAnim).not.to.be.undefined;
-	});
+  test('Should have method for animating diagonal pattern', () => {
+    const container = document.createElement('section');
+    const wpam = new WinPatternsAnimModule(container, 5, 5, 'diagonal');
+    expect(wpam.startDiagonallAnim).toBeDefined();
+  });
 });
