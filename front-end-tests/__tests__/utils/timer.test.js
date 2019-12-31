732ed4fbd31d6048ba24bcacf6085300593db8ee
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-31 12:51:31 +0200

    :heavy_check_mark: Migrating front end unit tests from Mocha to Jest - updated timer.test.js

--- front-end-tests/__tests__/utils/timer.test.js
+++ front-end-tests/__tests__/utils/timer.test.js
@@ -1,35 +1,29 @@
-/**
- * Created by Mihail on 10/14/2016.
- */
-'use strict';
-
 import Timer from '../../../src/utils/timer';
 import { EventsConsts } from '../../../src/events/events-consts';
-import { expect } from 'chai';
 
 describe('Timer module', () => {
 	test('Should create new Timer object', () => {
 		const timer = new Timer(document.querySelector('#timerContainer'), 5, EventsConsts.START_GAME, true);
-		expect(timer).not.to.be.undefined;
+		expect(timer).toBeDefined();
 	});
 
 	test('Should accept 3 parameters - seconds, eventName and isVisible', () => {
 		const timer = new Timer(document.querySelector('#timerContainer'), 5, EventsConsts.START_GAME, true);
-		expect(timer.hasOwnProperty('seconds')).to.be.truthy;
-		expect(timer.hasOwnProperty('eventName')).to.be.truthy;
-		expect(timer.hasOwnProperty('isVisible')).to.be.truthy;
+		expect(timer.hasOwnProperty('seconds')).toBeTruthy();
+		expect(timer.hasOwnProperty('eventName')).toBeTruthy();
+		expect(timer.hasOwnProperty('isVisible')).toBeTruthy();
 	});
 
 	test('Should accept 3 parameters types - number, string and boolean', () => {
 		const timer = new Timer(document.querySelector('#timerContainer'), 5, EventsConsts.START_GAME, true);
-		expect(timer.seconds).to.be.equal(5);
-		expect(timer.eventName).to.have.string(EventsConsts.START_GAME);
-		expect(timer.isVisible).to.be.true;
+		expect(timer.seconds).toEqual(5);
+		expect(timer.eventName).toContain(EventsConsts.START_GAME);
+		expect(timer.isVisible).toBeTruthy();
 	});
 
 	test('Should provide method for pulsating', () => {
 		const timer = new Timer(document.querySelector('#timerContainer'), 5, EventsConsts.START_GAME, true);
-		expect(timer.startCounting).not.to.be.undefined;
+		expect(timer.startCounting).toBeDefined();
 	});
 
 	test('Should animate pulse for given time and the to be hidden', () => {
@@ -37,19 +31,19 @@ describe('Timer module', () => {
 		const timer = new Timer(el, 5, EventsConsts.START_GAME, true);
 		timer.startCounting();
 		setTimeout(() => {
-			expect(timer.element.style.display).to.be.equal('none');
+			expect(timer.element.style.display).toEqual('none');
 		}, (timer.seconds) * 1000);
 	});
 
 	test('Should provide method hiding the timer', () => {
 		const timer = new Timer(document.querySelector('#timerContainer'), 5, EventsConsts.START_GAME, true);
-		expect(timer.hide).not.to.be.undefined;
+		expect(timer.hide).toBeDefined();
 	});
 
 	test('Should hide the Timer container whit the relevant method', () => {
 		const el = document.createElement('div');
 		const timer = new Timer(el, 5, EventsConsts.START_GAME, true);
 		timer.hide();
-		expect(timer.element.style.display).to.be.equal('none');
+		expect(timer.element.style.display).toEqual('none');
 	});
 });
