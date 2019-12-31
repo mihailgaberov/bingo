67f328ee77ea2e37fa461f0be37fbc38eb409c1f
mihailgaberov <mihail.gaberov@gmail.com>
2019-12-31 14:28:36 +0200

    :heavy_check_mark: Migrating front end unit tests from Mocha to Jest - updated flying-prize.test.js

--- front-end-tests/__tests__/winning/flying-prize.test.js
+++ front-end-tests/__tests__/winning/flying-prize.test.js
@@ -1,25 +1,19 @@
-/**
- * Created by Mihail on 11/10/2016.
- */
-
-'use strict';
-
 import FlyingPrize from '../../../src/winning/flying-prize';
 import Animator from '../../../src/utils/animator'
-import { expect } from 'chai';
 
 describe('FlyingPrize module', () => {
 	test('Should contain the prize sum to animate flying', () => {
 		const fp = new FlyingPrize(123);
-		expect(fp.sum).to.be.equal(123);
+		expect(fp.sum).toEqual(123);
 	});
 
 	test('Should contain a method for animating the prize', () => {
-		expect(FlyingPrize.animatePrizeFlying).not.to.be.undefined;
+		expect(FlyingPrize.animatePrizeFlying).toBeDefined();
 	});
 
 	test('Should use Animator method moveDiagonally', () => {
+		Animator.moveDiagonally = jest.fn();
 		FlyingPrize.animatePrizeFlying(123);
-		expect(Animator.moveDiagonally).to.be.calledOnce;
+		expect(Animator.moveDiagonally).toHaveBeenCalled();
 	});
 });
