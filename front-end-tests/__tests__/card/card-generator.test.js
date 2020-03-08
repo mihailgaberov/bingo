7d5c00a73b29145b439e6b4d8dd3113b35fa4852
mihailgaberov <mihail.gaberov@gmail.com>
2020-03-08 15:04:37 +0200

    :lipstick: Converted let --> const.

--- front-end-tests/__tests__/card/card-generator.test.js
+++ front-end-tests/__tests__/card/card-generator.test.js
@@ -2,7 +2,8 @@ import CardGenerator from '../../../src/card/card-generator';
 
 describe('Card Generator', () => {
 
-	let arrAmericanNumbers = [1, 2, 3, 4, 5,
+	const arrAmericanNumbers = [
+		1, 2, 3, 4, 5,
 		6, 7, 8, 9, 10,
 		11, 12, 13, 14, 15,
 		16, 17, 18, 19, 20,
