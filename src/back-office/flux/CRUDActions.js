cec997bae83ab825fc6dcae0b059486dab4c821b
mihailgaberov <mihail.gaberov@gmail.com>
2020-03-15 11:25:35 +0200

    :lipstick: Tidying up.

--- src/back-office/flux/CRUDActions.js
+++ src/back-office/flux/CRUDActions.js
@@ -1,78 +1,73 @@
-/**
- * Created by Mihail on 11/30/2016.
- */
-'use strict';
-
 /* @flow */
 
 import CRUDStore from './CRUDStore';
-import {List} from 'immutable';
+import { List } from 'immutable';
 
 const CRUDActions = {
-	create(newRecord: Object) {
-		CRUDStore.addRecord(newRecord);
-	},
+  create(newRecord: Object) {
+    CRUDStore.addRecord(newRecord);
+  },
 
-	delete(recordId: number) {
-		let data: List<Object> = CRUDStore.getData();
-		CRUDStore.deleteRecord(data.get(recordId), recordId);
-	},
+  delete(recordId: number) {
+    let data: List<Object> = CRUDStore.getData();
+    CRUDStore.deleteRecord(data.get(recordId), recordId);
+  },
 
-	updateRecord(recordId: number, newRecord: Object) {
-		CRUDStore.updateRecord(newRecord, recordId);
-	},
+  updateRecord(recordId: number, newRecord: Object) {
+    CRUDStore.updateRecord(newRecord, recordId);
+  },
 
-	updateField(recordId: number, key: string, value: string|number) {
-		let record = CRUDStore.getData().get(recordId);
-		record[key] = value;
-		CRUDStore.updateRecord(record, recordId);
-	},
+  updateField(recordId: number, key: string, value: string | number) {
+    let record = CRUDStore.getData().get(recordId);
+    record[key] = value;
+    CRUDStore.updateRecord(record, recordId);
+  },
 
-	_preSearchData: null,
+  _preSearchData: null,
 
-	startSearching() {
-		this._preSearchData = CRUDStore.getData();
-	},
+  startSearching() {
+    this._preSearchData = CRUDStore.getData();
+  },
 
-	search(e: Event) {
-		const target = ((e.target: any): HTMLInputElement);
-		const needle: string = target.value.toLowerCase();
-		if (!needle) {
-			CRUDStore.setData(this._preSearchData);
-			return;
-		}
-		const fields = CRUDStore.getSchema().map(item => item.id);
-		if (!this._preSearchData) {
-			return;
-		}
-		const searchdata = this._preSearchData.filter(row => {
-			for (let f = 0; f < fields.length; f++) {
-				if (row[fields[f]] !== undefined) {
-					if (row[fields[f]].toString().toLowerCase().indexOf(needle) > -1) {
-						return true;
-					}
-				}
-			}
-			return false;
-		});
-		CRUDStore.setData(searchdata, /* commit */ false);
-	},
+  search(e: Event) {
+    const target = ((e.target: any): HTMLInputElement);
+    const needle: string = target.value.toLowerCase();
+    if (!needle) {
+      CRUDStore.setData(this._preSearchData);
+      return;
+    }
+    const fields = CRUDStore.getSchema().map(item => item.id);
+    if (!this._preSearchData) {
+      return;
+    }
+    const searchdata = this._preSearchData.filter(row => {
+      for (let f = 0; f < fields.length; f++) {
+        if (row[fields[f]] !== undefined) {
+          if (row[fields[f]].toString().toLowerCase().indexOf(needle) > -1) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+    CRUDStore.setData(searchdata, /* commit */ false);
+  },
 
-	_sortCallback(a: (string|number), b: (string|number), descending: boolean): number {
-		let res: number = 0;
-		if (typeof a === 'number' && typeof b === 'number') {
-			res = a - b;
-		} else {
-			res = String(a).localeCompare(String(b));
-		}
-		return descending ? -1 * res : res;
-	},
+  _sortCallback(a: (string | number), b: (string | number), descending: boolean): number {
+    let res: number = 0;
+    if (typeof a === 'number' && typeof b === 'number') {
+      res = a - b;
+    } else {
+      res = String(a).localeCompare(String(b));
+    }
+    return descending ? -1 * res : res;
+  },
 
-	sort(key: string, descending: boolean) {
-		CRUDStore.setData(CRUDStore.getData().sort(
-			(a, b) => this._sortCallback(a[key], b[key], descending)
-		));
-	},
+  sort(key: string, descending: boolean) {
+    CRUDStore.setData(CRUDStore.getData().sort(
+      (a, b) => this._sortCallback(a[key], b[key], descending)
+    ));
+  },
 
 };
 
--- src/back-office/flux/CRUDStore.js
+++ src/back-office/flux/CRUDStore.js
@@ -1,11 +1,7 @@
-/**
- * Created by Mihail on 11/30/2016.
- */
-'use strict';
 /* @flow */
 
-import {EventEmitter} from 'fbemitter';
-import {List} from 'immutable';
+import { EventEmitter } from 'fbemitter';
+import { List } from 'immutable';
 import schema from '../schema';
 import ApiCtrl from '../../API/api-controller';
 import EventsConsts from '../components/EventsConsts';
@@ -14,131 +10,131 @@ let data: List<Object>;
 const emitter = new EventEmitter();
 
 const CRUDStore = {
-	init(storage) {
-		if (!storage) {
-			let initialRecord = {};
-			schema.forEach(item => initialRecord[item.id] = item.sample);
-			data = List([initialRecord]);
-		} else {
-			data = List(storage);
-		}
-	},
+  init(storage) {
+    if (!storage) {
+      let initialRecord = {};
+      schema.forEach(item => initialRecord[item.id] = item.sample);
+      data = List([initialRecord]);
+    } else {
+      data = List(storage);
+    }
+  },
 
-	getData(): List<Object> {
-		return data;
-	},
+  getData(): List<Object> {
+    return data;
+  },
 
-	getSchema(): Array<Object> {
-		return schema;
-	},
+  getSchema(): Array<Object> {
+    return schema;
+  },
 
-	setData(newData: List<Object>, commit: boolean = true) {
-		data = newData;
-		emitter.emit(EventsConsts.CHANGE);
-	},
+  setData(newData: List<Object>, commit: boolean = true) {
+    data = newData;
+    emitter.emit(EventsConsts.CHANGE);
+  },
 
-	addListener(eventType: string, fn: Function) {
-		emitter.addListener(eventType, fn);
-	},
+  addListener(eventType: string, fn: Function) {
+    emitter.addListener(eventType, fn);
+  },
 
-	getCount(): number {
-		return data.count();
-	},
+  getCount(): number {
+    return data.count();
+  },
 
-	getRecord(recordId: number): ?Object {
-		return data.get(recordId);
-	},
+  getRecord(recordId: number): ?Object {
+    return data.get(recordId);
+  },
 
-	addRecord(objRecord: Object) {
-		ApiCtrl.createPlayerPromise(objRecord).then((e) => {
-			if (e.isExisted) {
-				emitter.emit(EventsConsts.USER_EXISTS);
-				return false;
-			}
+  addRecord(objRecord: Object) {
+    ApiCtrl.createPlayerPromise(objRecord).then((e) => {
+      if (e.isExisted) {
+        emitter.emit(EventsConsts.USER_EXISTS);
+        return false;
+      }
 
-			if (!e.token) {
-				return false;
-			}
+      if (!e.token) {
+        return false;
+      }
 
-			CRUDStore.setData(CRUDStore.getData().unshift(objRecord));
-			emitter.emit(EventsConsts.CHANGE);
+      CRUDStore.setData(CRUDStore.getData().unshift(objRecord));
+      emitter.emit(EventsConsts.CHANGE);
 
-		});
-	},
+    });
+  },
 
-	deleteRecord(objRecord, recordId) {
-		if (objRecord.email) {
-			ApiCtrl.deletePlayerPromise(objRecord.email).then((e) => {
-				if (e.ok) {
-					CRUDStore.setData(CRUDStore.getData().remove(recordId));
-					emitter.emit(EventsConsts.CHANGE);
-				}
-			});
-		} else {
-			emitter.emit(EventsConsts.DELETE_ERROR);
-		}
-	},
+  deleteRecord(objRecord, recordId) {
+    if (objRecord.email) {
+      ApiCtrl.deletePlayerPromise(objRecord.email).then((e) => {
+        if (e.ok) {
+          CRUDStore.setData(CRUDStore.getData().remove(recordId));
+          emitter.emit(EventsConsts.CHANGE);
+        }
+      });
+    } else {
+      emitter.emit(EventsConsts.DELETE_ERROR);
+    }
+  },
 
-	updateRecord(objRecord, recordId) {
-		let objUpdated = {};
-		if (objRecord._id) {
-			objUpdated.email = objRecord.email;
-			objUpdated.name = objRecord.name;
-			objUpdated.balance = objRecord.balance;
-			objUpdated.wins = objRecord.wins;
-		} else {
-			objUpdated = objRecord;
-		}
-		if (objUpdated.email) {
-			ApiCtrl.updatePlayerDataPromise(objUpdated).then((newRecord) => {
-				if (newRecord) {
-					CRUDStore.setData(CRUDStore.getData().set(recordId, newRecord));
-					emitter.emit(EventsConsts.CHANGE);
-				}
-			});
-		} else {
-			emitter.emit(EventsConsts.UPDATE_ERROR);
-		}
-	},
+  updateRecord(objRecord, recordId) {
+    let objUpdated = {};
+    if (objRecord._id) {
+      objUpdated.email = objRecord.email;
+      objUpdated.name = objRecord.name;
+      objUpdated.balance = objRecord.balance;
+      objUpdated.wins = objRecord.wins;
+    } else {
+      objUpdated = objRecord;
+    }
+    if (objUpdated.email) {
+      ApiCtrl.updatePlayerDataPromise(objUpdated).then((newRecord) => {
+        if (newRecord) {
+          CRUDStore.setData(CRUDStore.getData().set(recordId, newRecord));
+          emitter.emit(EventsConsts.CHANGE);
+        }
+      });
+    } else {
+      emitter.emit(EventsConsts.UPDATE_ERROR);
+    }
+  },
 
-	checkLogin(objCredentials) {
-		if (!objCredentials.email || !objCredentials.password) {
-			emitter.emit(EventsConsts.LOGIN_FAILED);
-			return false;
-		}
+  checkLogin(objCredentials) {
+    if (!objCredentials.email || !objCredentials.password) {
+      emitter.emit(EventsConsts.LOGIN_FAILED);
+      return false;
+    }
 
-		ApiCtrl.loginAdminPromise(objCredentials).then((data) => {
-			if (data.token) {
-				emitter.emit(EventsConsts.LOGIN_SUCCESS);
+    ApiCtrl.loginAdminPromise(objCredentials).then((data) => {
+      if (data.token) {
+        emitter.emit(EventsConsts.LOGIN_SUCCESS);
 
-				// Save the credentials to local storage
-				localStorage.setItem('admin-token', data.token);
-			} else {
-				emitter.emit(EventsConsts.LOGIN_FAILED);
-			}
-		});
-	},
+        // Save the credentials to local storage
+        localStorage.setItem('admin-token', data.token);
+      } else {
+        emitter.emit(EventsConsts.LOGIN_FAILED);
+      }
+    });
+  },
 
-	isLoggedIn() {
-		let payload = null;
+  isLoggedIn() {
+    let payload = null;
 
-		let token = localStorage.getItem('admin-token');
+    let token = localStorage.getItem('admin-token');
 
-		if (token !== null) {
-			payload = token.split('.')[1];
-			payload = window.atob(payload);
-			payload = JSON.parse(payload);
+    if (token !== null) {
+      payload = token.split('.')[1];
+      payload = window.atob(payload);
+      payload = JSON.parse(payload);
 
-			return payload.exp > Date.now() / 1000;
-		} else {
-			return false;
-		}
-	},
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  },
 
-	logout() {
-		localStorage.removeItem('admin-token');
-		emitter.emit(EventsConsts.LOGOUT);
-	}
+  logout() {
+    localStorage.removeItem('admin-token');
+    emitter.emit(EventsConsts.LOGOUT);
+  }
 };
 
 export default CRUDStore
